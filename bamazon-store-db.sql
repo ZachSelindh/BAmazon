c44cc6a2041fad501f6cfb1568e4e67a2d3dd8c3
Zach Selindh <zachselindh@gmail.com>
2019-03-16 15:43:22 -0500

    Added DB and most JS functionality.

---
+++ bamazon-store-db.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE store (
+id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+product_name VARCHAR(50),
+department VARCHAR(50),
+price DECIMAL(5,2),
+stock INTEGER
+);
+
+INSERT INTO store (product_name, department, price, stock)
+VALUES ("Television", "Electronics", 300, 10),
+ ("Playstation", "Consoles", 250, 20),
+ ("XBox", "Consoles", 250, 5),
+ ("Nintendo Switch", "Consoles", 250, 25),
+ ("Super Mario", "Games", 60, 25),
+ ("Pokemon Let's Go", "Games", 65, 20),
+ ("Dark Souls 3", "Games", 35, 10),
+ ("Halo: Combat Evolved", "Games", 10, 30),
+ ("Civilization 5", "Games", 45, 20),
+ ("Halo: Combat Evolved", "Games", 10, 30)
+ ;
+
+SELECT * FROM store;
--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -3,4 +3,88 @@ by Zach Selindh */
 
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var colors = require("colors");
 
+var conn = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "bamazon"
+});
+
+conn.connect(function(err) {
+  if (err) throw err;
+  initMarket();
+});
+
+function initMarket() {
+  inquirer
+  .prompt([
+    {
+      message: "Welcome! Would you like to see what we have for sale? yes or no",
+      name: "welcome"
+    }
+  ]).then(function(answer){
+    if (answer.welcome == "yes") {
+      populateItems();
+    } else if (answer.welcome == "no") {
+      console.log("Thank you, come again!");
+      conn.end();
+    } else {
+      console.log("Please enter either 'yes' or 'no'.");
+      initMarket();
+    }
+  });
+};
+
+function populateItems() {
+  conn.query("SELECT * FROM store", function(err, data) {
+    if (err) throw err;
+    for (i=0; i < data.length; i++) {
+      console.log("ID: ".yellow + colors.green(data[i].id));
+      console.log("Product: ".yellow + data[i].product_name.green);
+      console.log("Department: ".yellow + data[i].department.green);
+      console.log("Price: ".yellow + colors.green(data[i].price));
+      console.log("Stock: ".yellow + colors.green(data[i].stock));
+      console.log("----------".red);
+    }
+    getChoices();
+  });
+};
+
+function getChoices() {
+  inquirer
+    .prompt([
+      {
+        message: "Please type the ID of the item you'd like to buy.",
+        name: "choice"
+      } ,
+      {
+        message: "How many would you like to buy?",
+        name: "quantity"
+      }
+    ]).then(function(answer) {
+      buyItems(answer.choice, answer.quantity);
+    });
+};
+
+
+function buyItems(choice, quantity) {
+  conn.query("SELECT * FROM store", function(err, data) {
+    sel = choice - 1;
+    if (err) throw err;
+    if (sel > data.length) {
+      console.log("That item does not exist.");
+      getChoices();
+    }
+    if (quantity > data[sel].stock) {
+      console.log("We don't have that many in stock.");
+      getChoices();
+    } else {
+      let newStock = data[sel].stock - quantity;
+      conn.query("SELECT ? FROM store" )
+    }
+
+  });
+};
